d631fc9c1abee2db5a3997198bb2da71fd9d8674
George Fang <57463760+georgef7@users.noreply.github.com>
2025-03-15 19:42:10 -0400

    Go Service for Marta API

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/georgef7/marta-train-go-api
+
+go 1.24.1
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/rs/cors v1.11.1 // indirect
+)
---
+++ martaapi.go
@@ -0,0 +1,95 @@
+// Author: George Fang
+// Note that this project is not enorsed or affliated with MARTA in any way.
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+	"github.com/rs/cors"
+	"slices"
+)
+
+type trainArrival []struct {
+	Destination    string `json:"DESTINATION"`
+	Direction      string `json:"DIRECTION"`
+	EventTime      string `json:"EVENT_TIME"`
+	IsRealTime     string `json:"IS_REALTIME"`
+	Line           string `json:"LINE"`
+	NextArrival    string `json:"NEXT_ARR"`
+	Station        string `json:"STATION"`
+	TrainID        string `json:"TRAIN_ID"`
+	WaitingSeconds string `json:"WAITING_SECONDS"`
+	WaitingTime    string `json:"WAITING_TIME"`
+	Delay          string `json:"DELAY"`
+	Latitude       string `json:"LATITUDE"`
+	Longitude      string `json:"LONGITUDE"`
+}
+
+func main() {
+	loadEnvError := godotenv.Load(".env.local")
+
+	if loadEnvError != nil {
+		log.Println("Cannot load environment variables from .env.local file")
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		allowedOrgins := strings.Split(os.Getenv("ALLOWED_ORIGINS"), ",")
+
+		origin := r.Header.Get("Origin")
+		log.Println("A request has been received. The origin of the request is:", origin)
+
+		// func Contains(s, v) reports whether v is present in s
+		allowAccess := slices.Contains(allowedOrgins, origin)
+
+		if !allowAccess {
+			http.Error(w, "Access denied. Origin not accepted.", http.StatusForbidden)
+			return
+		}
+
+		apiKey := os.Getenv("API_KEY")
+
+		// add apiKey to URL
+		martaURL := fmt.Sprintf("https://developerservices.itsmarta.com:18096/itsmarta/railrealtimearrivals/developerservices/traindata?apiKey=%s", apiKey)
+
+		// Get train data from MARTA
+		response, httpGetErr := http.Get(martaURL)
+		if httpGetErr != nil {
+			log.Println("Something went wrong during GET.", httpGetErr)
+		}
+
+		// Read response
+		body, readErr := io.ReadAll(response.Body)
+		if readErr != nil {
+			log.Println("Something went wrong reading reqest", readErr)
+		}
+
+		var trainArrivalData trainArrival
+		unmarshalErr := json.Unmarshal(body, &trainArrivalData)
+		if unmarshalErr != nil {
+			log.Println("Error unmarshaling train arrival data", unmarshalErr)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		encodeErr := json.NewEncoder(w).Encode(trainArrivalData)
+		if encodeErr != nil {
+			log.Println("Error encoding train arrival data", encodeErr)
+		}
+	})
+	log.Println("Train Arrival Service API Started!")
+
+	// cors.Default() setup the middleware with default options being
+	// all origins accepted with simple methods (GET, POST). See
+	// documentation below for more options.
+	handler := cors.Default().Handler(mux)
+	http.ListenAndServe(":8080", handler)
+}
